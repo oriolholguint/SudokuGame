254dc27923fdd2e13c94687337b47e55b7184c0e
Oriol Holguin <oriolholguint@gmail.com>
2020-12-17 14:34:41 +0100

    Creación método getRow (Input), recoge valor carácter

--- src/sudokugame/Input.java
+++ src/sudokugame/Input.java
@@ -1,9 +1,33 @@
 package sudokugame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author (oriolholguint@gmail.com)
  */
-public class Input {
+public class Input 
+{
+	
+	public char getRow()
+	{
+		String preLetter;
+		char letter = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		preLetter = sc.nextLine();
+		
+		if(preLetter.length() == 1)
+		{
+			letter = preLetter.charAt(0);
+		}
+		else
+		{
+			System.err.println("Introduce solo un carácter, porfavor. =)");
+		}
+		
+		return letter;
+	}
+	
 	
 }
