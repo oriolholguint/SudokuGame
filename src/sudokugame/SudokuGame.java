ccdd1daceeb461f9d9f45a8071e094754e938b4e
cep <cep@cepnet0.net>
2020-12-17 11:05:06 +0100

    Creacion de repositorio / Creacion de la clase main (Uri)

---
+++ src/sudokugame/SudokuGame.java
@@ -0,0 +1,20 @@
+package sudokugame;
+
+/**
+ *
+ * @author cep
+ */
+public class SudokuGame 
+{
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) 
+	{
+		
+		
+		
+	}
+	
+}
