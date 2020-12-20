0de2f1a7e85e7801fc7c44ab7e83ea8f83337a8d
Oriol Holguin <oriolholguint@gmail.com>
2020-12-20 12:40:05 +0100

    Añado static a métodos clase Input

--- src/sudokugame/Input.java
+++ src/sudokugame/Input.java
@@ -9,44 +9,44 @@ import java.util.Scanner;
 public class Input 
 {
 	
-	public char getChar()
-	{
-            String preLetter;
-            char letter = 0;
+    public static char getChar()
+    {
+        String preLetter;
+        char letter = 0;
 
-            Scanner sc = new Scanner(System.in);
-            preLetter = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        preLetter = sc.nextLine();
 
-            if(preLetter.length() == 1)
-            {
-                letter = preLetter.charAt(0);
-            }
-            else
-            {
-                System.err.println("Introduce solo un carácter, porfavor. =)");
-            }
-
-            return letter;
-	}
-	
-	public int getInt()
-	{
-            int number;
-
-            Scanner sc = new Scanner(System.in);
-            number = sc.nextInt();
-
-            return number;
-	}
-        
-        public String getString()
+        if(preLetter.length() == 1)
         {
-            String word;
-            
-            Scanner sc = new Scanner(System.in);
-            word = sc.nextLine();
-            
-            return word;            
+            letter = preLetter.charAt(0);
         }
+        else
+        {
+            System.err.println("Introduce solo un carácter, porfavor. =)");
+        }
+
+        return letter;
+    }
+
+    public static int getInt()
+    {
+        int number;
+
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+
+        return number;
+    }
+
+    public static String getString()
+    {
+        String word;
+
+        Scanner sc = new Scanner(System.in);
+        word = sc.nextLine();
+
+        return word;            
+    }
 	
 }
