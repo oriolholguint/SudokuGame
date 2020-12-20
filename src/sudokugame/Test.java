335b079a11871c26125c801116eeb3f95917abe4
Oriol Holguin <oriolholguint@gmail.com>
2020-12-20 17:58:15 +0100

    Añado una prueba en clase Test (Uri)

--- src/sudokugame/Test.java
+++ src/sudokugame/Test.java
@@ -1,5 +1,8 @@
 package sudokugame;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.DecimalDV;
+import java.text.DecimalFormat;
+
 /**
  *
  * @author (oriolholguint@gmail.com)
@@ -11,22 +14,269 @@ public class Test
     
     public static void main(String[] args) 
     {
-        System.out.println((int) (Math.random() * 4));
         int boardPos [][] = new int [4][4];
-        boolean repeat = false;
-        int col;
-        int fila;
-
+        boolean repeat;
+        int col, fila;
+        int randomNumber = 0;
+        boolean repeatValue = false;
+        
         for (int i = 0; i < CANT_CASILLAS; i++)
         {    
             do
             {
-                repeat = false;
+                //System.out.println(res);
                 col = (int) (Math.random() * 4);
                 fila = (int) (Math.random() * 4);
-                if (boardPos[col][fila] == 0)
+                int res = Integer.valueOf(String.valueOf(fila + String.valueOf(col)));
+                repeat = false;
+                if (boardPos[fila][col] == 0)
                 {
-                    boardPos[col][fila] = (int)(Math.random() * 4 + 1);
+                    while (repeatValue)
+                    {
+                        switch (res)
+                        {
+                            case 0:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[0][1] == randomNumber || boardPos[0][2] == randomNumber || boardPos[0][3] == randomNumber ||
+                                    boardPos[1][0] == randomNumber || boardPos[2][0] == randomNumber || boardPos[3][0] == randomNumber ||
+                                    boardPos[1][1] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 1: 
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[0][0] == randomNumber || boardPos[0][2] == randomNumber || boardPos[0][3] == randomNumber ||
+                                    boardPos[1][1] == randomNumber || boardPos[2][1] == randomNumber || boardPos[3][1] == randomNumber ||
+                                    boardPos[1][0] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 2:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[0][0] == randomNumber || boardPos[0][1] == randomNumber || boardPos[0][3] == randomNumber ||
+                                    boardPos[1][2] == randomNumber || boardPos[2][2] == randomNumber || boardPos[3][2] == randomNumber ||
+                                    boardPos[1][3] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;    
+                            case 3:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[0][0] == randomNumber || boardPos[0][1] == randomNumber || boardPos[0][2] == randomNumber ||
+                                    boardPos[1][3] == randomNumber || boardPos[2][3] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[1][2] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break; 
+                            case 10:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[0][0] == randomNumber || boardPos[2][0] == randomNumber || boardPos[3][0] == randomNumber ||
+                                    boardPos[1][1] == randomNumber || boardPos[1][2] == randomNumber || boardPos[1][3] == randomNumber ||
+                                    boardPos[0][1] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break; 
+                            case 11:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[1][0] == randomNumber || boardPos[1][2] == randomNumber || boardPos[1][3] == randomNumber ||
+                                    boardPos[0][1] == randomNumber || boardPos[2][1] == randomNumber || boardPos[3][1] == randomNumber ||
+                                    boardPos[0][0] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 12:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[1][0] == randomNumber || boardPos[1][1] == randomNumber || boardPos[1][3] == randomNumber ||
+                                    boardPos[0][2] == randomNumber || boardPos[2][2] == randomNumber || boardPos[3][2] == randomNumber ||
+                                    boardPos[0][3] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 13:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[1][0] == randomNumber || boardPos[1][1] == randomNumber || boardPos[1][2] == randomNumber ||
+                                    boardPos[0][3] == randomNumber || boardPos[2][3] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[0][2] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 20:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[2][1] == randomNumber || boardPos[2][2] == randomNumber || boardPos[2][3] == randomNumber ||
+                                    boardPos[0][0] == randomNumber || boardPos[1][0] == randomNumber || boardPos[3][0] == randomNumber ||
+                                    boardPos[3][1] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 21:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[2][0] == randomNumber || boardPos[2][2] == randomNumber || boardPos[2][3] == randomNumber ||
+                                    boardPos[0][1] == randomNumber || boardPos[1][1] == randomNumber || boardPos[3][1] == randomNumber ||
+                                    boardPos[3][0] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 22:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[2][0] == randomNumber || boardPos[2][1] == randomNumber || boardPos[2][3] == randomNumber ||
+                                    boardPos[0][2] == randomNumber || boardPos[1][2] == randomNumber || boardPos[3][2] == randomNumber ||
+                                    boardPos[3][3] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 23:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[2][0] == randomNumber || boardPos[2][1] == randomNumber || boardPos[2][2] == randomNumber ||
+                                    boardPos[0][3] == randomNumber || boardPos[1][3] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[3][2] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 30:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[3][1] == randomNumber || boardPos[3][2] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[0][0] == randomNumber || boardPos[1][0] == randomNumber || boardPos[2][0] == randomNumber ||
+                                    boardPos[2][1] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 31:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[3][0] == randomNumber || boardPos[3][2] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[0][1] == randomNumber || boardPos[1][1] == randomNumber || boardPos[2][1] == randomNumber ||
+                                    boardPos[2][0] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 32:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[3][0] == randomNumber || boardPos[3][1] == randomNumber || boardPos[3][3] == randomNumber ||
+                                    boardPos[0][2] == randomNumber || boardPos[1][2] == randomNumber || boardPos[2][2] == randomNumber ||
+                                    boardPos[2][3] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                            case 33:
+                            {
+                                randomNumber = (int)(Math.random() * 4 + 1);
+                                if (boardPos[3][0] == randomNumber || boardPos[3][1] == randomNumber || boardPos[3][2] == randomNumber ||
+                                    boardPos[0][3] == randomNumber || boardPos[1][3] == randomNumber || boardPos[2][3] == randomNumber ||
+                                    boardPos[2][2] == randomNumber)
+                                {
+                                    repeatValue = true;
+                                }
+                                else
+                                {
+                                    boardPos[fila][col] = randomNumber;
+                                }
+                            }
+                            break;
+                        }
+                    }
                 }
                 else
                 {
